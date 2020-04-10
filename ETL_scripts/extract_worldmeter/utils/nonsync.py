bf42d53ef798b62acf1e438791d81e62bbd05425
royassis <royassisg@gmail.com>
2020-04-10 14:02:44 +0300

    async logging

--- ETL_scripts/extract_worldmeter/utils/nonsync.py
+++ ETL_scripts/extract_worldmeter/utils/nonsync.py
@@ -6,19 +6,15 @@ from datetime import datetime
 from ETL_scripts.extract_worldmeter.settings import  *
 
 
-
 async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
-    """GET request wrapper to fetch page HTML.
-
-    kwargs are passed to `session.request()`.
-    """
     resp = await session.request(method="GET", url=url, **kwargs)
     resp.raise_for_status()
     html = await resp.text()
     return html
 
-async def to_file(url,session) -> None:
+async def to_file(url,session,i,urls_len) -> None:
 
+    logger.info(f'fecting file {i} from {urls_len}')
     html = await fetch_html(url,session)
 
     container = pd.read_html(html, match=READ_HTML_MATCH_PARAM)
@@ -37,15 +33,17 @@ async def to_file(url,session) -> None:
 
 async def bulk_crawl_and_write( urls: list, **kwargs) -> None:
     """Crawl & write concurrently to `file` for multiple `urls`."""
+    urls_len = len(urls)
     async with ClientSession() as session:
         tasks = []
-        for url in urls:
+        for i,url in enumerate(urls):
             tasks.append(
-                to_file(url=url, session=session, **kwargs)
+                to_file(url=url, session=session, i=i, urls_len= urls_len, **kwargs)
             )
         await asyncio.gather(*tasks)
 
 def main(urls):
+
     asyncio.run(bulk_crawl_and_write(urls=urls))
 
 if __name__ == '__main__':
