e1c02c98837d85cfb23694aedbed3d546574862e
royassis <royassisg@gmail.com>
2020-04-10 13:01:35 +0300

    cmt

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# TODO
-# Activate all processes
+from ETL_scripts.extract_worldmeter.extract_worldmeter_data import main as extract_worldmeter_data
+from ETL_scripts.extract_gov_data import main as extract_gov_data
+from ETL_scripts.transform_worldmeter_data import main as transform_worldmeter_data
+
+extract_worldmeter_data()
+transform_worldmeter_data()
+extract_gov_data()
+
