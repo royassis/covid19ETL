5ed62c141cf34c4cfed3796373410d7b95787026
royassis <royassisg@gmail.com>
2020-04-11 15:04:01 +0300

    gsheets read

--- ETL_scripts/extract_gsheets/functions.py
+++ ETL_scripts/extract_gsheets/functions.py
@@ -6,6 +6,11 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pandas as pd
 
+THISDIR = os.path.dirname(__file__)
+CREDS_PATH = os.path.join(THISDIR, 'credentials.json')
+TOKEN_PATH = os.path.join(THISDIR, 'token.pickle')
+
+
 def values_to_df(result):
     header = result.get('values', [])[0]
     values = result.get('values', [])[1:]
@@ -32,8 +37,8 @@ def create_service(SCOPES):
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -41,10 +46,10 @@ def create_service(SCOPES):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                CREDS_PATH, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(TOKEN_PATH, 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
