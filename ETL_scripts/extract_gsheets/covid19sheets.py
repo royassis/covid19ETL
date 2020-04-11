88fd3daee3f55850db2a23de73c153c51f67d62a
royassis <royassisg@gmail.com>
2020-04-11 16:33:42 +0300

    Merge remote-tracking branch 'origin/master'

--- ETL_scripts/extract_gsheets/covid19sheets.py
+++ ETL_scripts/extract_gsheets/covid19sheets.py
@@ -6,22 +6,21 @@ from .functions import *
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-ser = create_service(SCOPES)
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1Y-ieLWMDzFzzeJKW-SygD90dBH80d4x0db8I3UFNj_c'
+SPREADSHEET_ID = '1Y-ieLWMDzFzzeJKW-SygD90dBH80d4x0db8I3UFNj_c'
 
 SHEET_NAME = 'חולים מאומתים/ישוב'
 RANGE = 'A3:U'
 SAMPLE_RANGE_NAME = f'{SHEET_NAME}!{RANGE}'
 
 def main(outpath = None):
+    print(__file__, 'is running')
 
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
     service = create_service(SCOPES)
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                                 range=SAMPLE_RANGE_NAME).execute()
 
     df = values_to_df(result)
