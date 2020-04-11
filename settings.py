c70fa8c695820c1ab16d0f38608ddc86b31ecc18
royassis <royassisg@gmail.com>
2020-04-11 16:28:08 +0300

    changes

--- settings.py
+++ settings.py
@@ -6,15 +6,12 @@ current_dir = os.path.dirname(__file__)
 # Paths and Dirs
 WORLDMETER_DATA = os.path.join(current_dir,'DW/raw_data/worldmeter')
 GOV_RESOURCE_PATH = os.path.join(current_dir,'resources/csv/gov_resource.csv')
-CUTOFF_DATE = '2020-02-10'
 
 MAPPER_PATH = os.path.join(current_dir,'resources/csv/column_remapper.csv')
 mapper = pd.read_csv(MAPPER_PATH, index_col ='key', usecols = ['key', 'value'])
 column_remapper = mapper.iloc[:,0]
 
-URLS_PATH  = os.path.join(current_dir,'resources/csv/urls.csv')
-urls = pd.read_csv(URLS_PATH, index_col ='id')
-GOVERNMENT_RESPONSE_URL = urls.loc[4,'url']
+GOVERNMENT_RESPONSE_URL = 'https://www.bsg.ox.ac.uk/sites/default/files/OxCGRT_Download_latest_data.xlsx'
 
 POPULATION_CSV_PATH = os.path.join(current_dir,'DW/raw_data/population_data.csv')
 
