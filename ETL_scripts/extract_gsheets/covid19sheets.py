a1032227dcc62602369eee75f2905f2accde3de2
royassis <royassisg@gmail.com>
2020-04-11 14:52:07 +0300

    gsheets read

---
+++ ETL_scripts/extract_gsheets/covid19sheets.py
@@ -0,0 +1,33 @@
+from functions import *
+
+# Enable sheets api at: https://developers.google.com/sheets/api/quickstart/python
+# Choose desktop app
+# Download the client configuration file and place in the folder of this script
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+ser = create_service(SCOPES)
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1Y-ieLWMDzFzzeJKW-SygD90dBH80d4x0db8I3UFNj_c'
+
+SHEET_NAME = 'חולים מאומתים/ישוב'
+RANGE = 'A3:U'
+SAMPLE_RANGE_NAME = f'{SHEET_NAME}!{RANGE}'
+
+OUTPATH = 'results.csv'
+
+def main():
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    service = create_service(SCOPES)
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+
+    df = values_to_df(result)
+    df.to_csv(OUTPATH)
+
+
+if __name__ == '__main__':
+    main()
