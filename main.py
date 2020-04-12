c82d2a2339d1f68dd05c887e3ebe03e63b80bcbf
royassis <royassisg@gmail.com>
2020-04-12 10:06:06 +0300

    fixed something

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from ETL_scripts.transform_worldmeter_data import main as transform_worldmeter_d
 from ETL_scripts.extract_gov_data import main as extract_gov_data
 from ETL_scripts.extract_gsheets.covid19sheets import main as extract_sheet_data
 
-DATA_DIR = r'D:\PycharmProjects\covid19ETL\DW\raw_data\worldmeter'
-CUTOFF_DATE = '2020-02-10'
 
-extract_worldmeter_data(DATA_DIR)
-transform_worldmeter_data(DATA_DIR,cutoffdate = CUTOFF_DATE)
-a=extract_gov_data(r'D:\PycharmProjects\covid19ETL\DW\raw_data\gov_data')
+extract_worldmeter_data(r'D:\PycharmProjects\covid19ETL\DW\raw_data\worldmeter')
+transform_worldmeter_data(r'D:\PycharmProjects\covid19ETL\DW\raw_data\worldmeter',
+                          outpath = 'D:\PycharmProjects\covid19ETL\DW\loaded_data',
+                          cutoffdate = '2020-02-10')
+extract_gov_data(r'D:\PycharmProjects\covid19ETL\DW\raw_data\gov_data')
 extract_sheet_data(r'D:\PycharmProjects\covid19ETL\DW\raw_data\gov_data')
 
